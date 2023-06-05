435c44c563800d9617c5c346b021f9940260dc03
mesharikw85 <mkwg85@gmail.com>
2023-06-06 00:27:35 +0300

    finsh hiking trip

--- src/components/TripDetail.js
+++ src/components/TripDetail.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import tripsData from '../tripsData';
+import React from "react";
+import tripsData from "../tripsData";
+import { useParams, Navigate } from "react-router-dom";
 
 function TripDetail() {
-  const trip = tripsData[0];
+  const { tripSlug } = useParams();
+  const trip = tripsData.find((trip) => trip.slug == tripSlug);
+  if (!trip) {
+    return <Navigate to="/" />;
+  }
   return (
     <div className="modal-dialog modal-xl">
       <div className="modal-content">
